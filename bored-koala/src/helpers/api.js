bb6a792a818824e8bf2c2d361b798f8157d835fd
kesh gurung <k_grg@hotmail.com>
2021-12-17 11:46:26 +0000

    get all endpoints in single page

--- bored-koala/src/helpers/api.js
+++ bored-koala/src/helpers/api.js
@@ -1,24 +1,26 @@
 import axios from 'axios'
 
-export const getMovieData = async () => {
-  // export const getData = async () => {
-  //   const config = {
-  //     method: 'get',
-  //     url: `https://motivational-quotes1.p.rapidapi.com/motivation`,
-  //     headers: {
-  //       'content-type': 'application/json',
-  //       'x-rapidapi-host': 'motivational-quotes1.p.rapidapi.com',
-  //       'x-rapidapi-key': 'aeb717ee1emsha514d9d6575c369p1ee3ffjsne764f2a7e700',
-  //     },
-  //     body: {
-  //       key1: 'value',
-  //       key2: 'value',
-  //     },
-  //   }
-  //   const response = await axios(config)
-  //   return response.data
-  // }
+export const getJokeData = async () => {
+  const options = {
+    method: 'GET',
+    url: 'https://joke-generator.p.rapidapi.com/generate-joke',
+    headers: {
+      'x-rapidapi-host': 'joke-generator.p.rapidapi.com',
+      'x-rapidapi-key': 'aeb717ee1emsha514d9d6575c369p1ee3ffjsne764f2a7e700',
+    },
+  }
 
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data)
+    })
+    .catch(function (error) {
+      console.error(error)
+    })
+}
+
+export const getMusicData = async () => {
   const options = {
     method: 'GET',
     url: 'https://genius.p.rapidapi.com/search',
@@ -38,21 +40,24 @@ export const getMovieData = async () => {
       console.error(error)
     })
 }
-// const options = {
-//   method: 'GET',
-//   url: 'https://movie-database-imdb-alternative.p.rapidapi.com/',
-//   params: { s: 'Avengers Endgame', page: '1', r: 'json' },
-//   headers: {
-//     'x-rapidapi-host': 'movie-database-imdb-alternative.p.rapidapi.com',
-//     'x-rapidapi-key': 'aeb717ee1emsha514d9d6575c369p1ee3ffjsne764f2a7e700',
-//   },
-// }
 
-// axios
-//   .request(options)
-//   .then(function (response) {
-//     console.log(response.data)
-//   })
-//   .catch(function (error) {
-//     console.error(error)
-//   })
+export const getMovieData = async () => {
+  const options = {
+    method: 'GET',
+    url: 'https://imdb8.p.rapidapi.com/auto-complete',
+    params: { q: 'game of thr' },
+    headers: {
+      'x-rapidapi-host': 'imdb8.p.rapidapi.com',
+      'x-rapidapi-key': 'aeb717ee1emsha514d9d6575c369p1ee3ffjsne764f2a7e700',
+    },
+  }
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data)
+    })
+    .catch(function (error) {
+      console.error(error)
+    })
+}
--- bored-koala/src/pages/Home.js
+++ bored-koala/src/pages/Home.js
@@ -1,42 +1,65 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { getMovieData, getJokeData, getMusicData } from '../helpers/api'
 
 const Home = () => {
+  const [click, setClick] = useState(false)
+  const [value, setValue] = useState(null)
   const handleClick = (event) => {
-    return event.target.value
+    const newVal = event.target.value
+    setClick(true)
+    setValue(newVal)
   }
 
+  useEffect(() => {
+    // console.log(value)
+    if (value === 'browse movie') {
+      getMovieData()
+    } else if (value === 'search music') {
+      getMusicData()
+    } else if (value === 'get random') {
+      getJokeData()
+    }
+  }, [value])
+
   return (
-    <div>
-      <h1>Welcome to Bored Koala</h1>
-      <h3>what would you like to do today</h3>
-      <Link to={'/jokelist'}>
-        <button
-          onClick={handleClick}
-          className='favorite styled'
-          type='button'
-          value='browse movie'
-        >
-          Browse Movie{' '}
-        </button>
-      </Link>
-      <button
-        onClick={handleClick}
-        className='favorite styled'
-        type='button'
-        value='search movie'
-      >
-        Search Music{' '}
-      </button>
-      <button
-        onClick={handleClick}
-        className='favorite styled'
-        type='button'
-        value='get random'
-      >
-        Get random jokes/quotes
-      </button>
-    </div>
+    <>
+      {!click ? (
+        <div>
+          <h1>Welcome to Bored Koala</h1>
+          <h3>what would you like to do today</h3>
+          {/* <Link to={'/jokelist'}> */}
+          <button
+            onClick={handleClick}
+            className='favorite styled'
+            type='button'
+            value='browse movie'
+          >
+            Browse Movie{' '}
+          </button>
+          {/* </Link> */}
+          <button
+            onClick={handleClick}
+            className='favorite styled'
+            type='button'
+            value='search music'
+          >
+            Search Music{' '}
+          </button>
+          <button
+            onClick={handleClick}
+            className='favorite styled'
+            type='button'
+            value='get random'
+          >
+            Get random jokes/quotes
+          </button>
+        </div>
+      ) : (
+        console.log(value)
+      )}
+    </>
   )
 }
 
